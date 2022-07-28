d531523d5c0490577dae61f6eb5aaa10f20920c9
Diego Bertolo <dbertolo86@gmail.com>
2022-07-28 10:29:39 -0300

    Consulta Simples com query methods

--- src/main/java/com/db/workshopmongo/resources/PostResource.java
+++ src/main/java/com/db/workshopmongo/resources/PostResource.java
@@ -1,6 +1,8 @@
 package com.db.workshopmongo.resources;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,9 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.db.workshopmongo.domain.Post;
+import com.db.workshopmongo.resources.util.URL;
 import com.db.workshopmongo.service.PostService;
 
 @RestController
@@ -28,5 +32,14 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 		
 	}
+	
+	
+   @RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+   public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text) {
+	   text = URL.decodeParam(text);
+	   List<Post> list = service.findByTitle(text);
+	   return ResponseEntity.ok().body(list);
+   }
+	
 	
 }
--- src/main/java/com/db/workshopmongo/service/PostService.java
+++ src/main/java/com/db/workshopmongo/service/PostService.java
@@ -1,6 +1,7 @@
 package com.db.workshopmongo.service;
 
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,4 +24,9 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		
 	}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
+	
 }
